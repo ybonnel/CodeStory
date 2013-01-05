cc901f340e8b8f21604fdaa1192a1e5d1c0a8f50
ybonnel <ybonnel@gmail.com>
2013-01-05 20:51:22 +0100

    Fix stopServeur

--- stopServeur.sh
+++ stopServeur.sh
@@ -1,1 +1,1 @@
-ps -ef | grep java | grep code-story | grep -v grep | while read a b c ; do kill -9 ; done
+ps -ef | grep java | grep code-story | grep -v grep | while read a b c ; do kill -9 $b; done
