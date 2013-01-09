418f90aefc7e8eb49381872cad353cfacc7ccd68
ybonnel <ybonnel@gmail.com>
2013-01-09 20:21:13 +0100

    Add manage of scalaskel change

--- src/main/java/fr/ybonnel/codestory/query/ChangeScalaskelQueryHandler.java
+++ src/main/java/fr/ybonnel/codestory/query/ChangeScalaskelQueryHandler.java
@@ -1,20 +1,67 @@
 package fr.ybonnel.codestory.query;
 
 
-import com.google.common.collect.Lists;
 import com.google.gson.GsonBuilder;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ChangeScalaskelQueryHandler extends AbstractQueryHandler {
     @Override
     public String getResponse(String query, String path, HttpServletRequest request) throws Exception {
-        return new GsonBuilder().create().toJson(getChanges(1));
+
+        Matcher matcher = Pattern.compile("/scalaskel/change/(\\d+)").matcher(path);
+        if (matcher.matches()) {
+            return new GsonBuilder().create().toJson(getChanges(Integer.parseInt(matcher.group(1))));
+        }
+        return null;
     }
 
     private List<Change> getChanges(int cents) {
-        return Lists.newArrayList(new Change(1, null, null, null));
+
+        List<Change> changes = new ArrayList<Change>();
+        completeChanges(cents, null, changes, null);
+        return changes;
+    }
+
+    private void completeChanges(int cents, Change currentChange, List<Change> changes, Cent lastMoney) {
+        if (cents == 0) {
+            changes.add(currentChange);
+        }
+        for (Cent monay : Cent.values()) {
+            if (lastMoney == null ||
+                    monay.getValue() >= lastMoney.getValue()) {
+                if (monay.canPay(cents)) {
+                    Change change = new Change(currentChange);
+                    change.pay(monay);
+                    completeChanges(cents - monay.getValue(), change, changes, monay);
+                }
+            }
+        }
+    }
+
+    private enum Cent {
+        FOO(1),
+        BAR(7),
+        QIX(11),
+        BAZ(21);
+
+        private int value;
+
+        private Cent(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public boolean canPay(int cents) {
+            return cents >= value;
+        }
     }
 
     private class Change {
@@ -23,11 +70,37 @@ public class ChangeScalaskelQueryHandler extends AbstractQueryHandler {
         private Integer qix;
         private Integer baz;
 
-        private Change(Integer foo, Integer bar, Integer qix, Integer baz) {
+        public Change(Integer foo, Integer bar, Integer qix, Integer baz) {
             this.foo = foo;
             this.bar = bar;
             this.qix = qix;
             this.baz = baz;
+        }
+
+        public Change(Change change) {
+            if (change != null) {
+                this.foo = change.foo;
+                this.bar = change.bar;
+                this.qix = change.qix;
+                this.baz = change.baz;
+            }
+        }
+
+        public void pay(Cent cent) {
+            switch (cent) {
+                case FOO:
+                    this.foo = this.foo == null ? 1 : this.foo + 1;
+                    break;
+                case BAR:
+                    this.bar = this.bar == null ? 1 : this.bar + 1;
+                    break;
+                case QIX:
+                    this.qix = this.qix == null ? 1 : this.qix + 1;
+                    break;
+                case BAZ:
+                    this.baz = this.baz == null ? 1 : this.baz + 1;
+                    break;
+            }
         }
     }
 }
--- src/test/java/fr/ybonnel/codestory/ScalaskelTest.java
+++ src/test/java/fr/ybonnel/codestory/ScalaskelTest.java
@@ -20,5 +20,29 @@ public class ScalaskelTest extends WebServerTestUtil {
         assertEquals("[{\"foo\":1}]", response.getText());
     }
 
+    @Test
+    public void should_answer_to_7cent() throws IOException, SAXException {
+        WebConversation wc = new WebConversation();
+        WebResponse response = wc.getResponse(getURL() + "/scalaskel/change/7");
+        assertEquals(200, response.getResponseCode());
+        assertEquals("[{\"foo\":7},{\"bar\":1}]", response.getText());
+    }
+
+    @Test
+    public void should_answer_to_11cent() throws IOException, SAXException {
+        WebConversation wc = new WebConversation();
+        WebResponse response = wc.getResponse(getURL() + "/scalaskel/change/11");
+        assertEquals(200, response.getResponseCode());
+        assertEquals("[{\"foo\":11},{\"foo\":4,\"bar\":1},{\"qix\":1}]", response.getText());
+    }
+
+    @Test
+    public void should_answer_to_21cent() throws IOException, SAXException {
+        WebConversation wc = new WebConversation();
+        WebResponse response = wc.getResponse(getURL() + "/scalaskel/change/21");
+        assertEquals(200, response.getResponseCode());
+        assertEquals("[{\"foo\":21},{\"foo\":14,\"bar\":1},{\"foo\":10,\"qix\":1},{\"foo\":7,\"bar\":2},{\"foo\":3,\"bar\":1,\"qix\":1},{\"bar\":3},{\"baz\":1}]", response.getText());
+    }
+
 
 }
