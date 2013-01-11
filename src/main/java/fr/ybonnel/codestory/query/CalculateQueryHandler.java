c2dc99d658f5e480497c095edf43f7bcfb7dba25
ybonnel <ybonnel@gmail.com>
2013-01-11 21:47:54 +0100

    Comma management

--- src/main/java/fr/ybonnel/codestory/query/CalculateQueryHandler.java
+++ src/main/java/fr/ybonnel/codestory/query/CalculateQueryHandler.java
@@ -1,6 +1,12 @@
 package fr.ybonnel.codestory.query;
 
 
+import com.google.common.base.Throwables;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -8,15 +14,21 @@ public class CalculateQueryHandler extends AbstractQueryHandler {
 
     private Pattern patternPlus = Pattern.compile("(\\d+)\\+(\\d+)");
     private Pattern patternParenthesis = Pattern.compile("\\((.*)\\)");
-    private Pattern patternMultiple = Pattern.compile("(\\d+)\\*(\\d+)");
+    private Pattern patternMultiple = Pattern.compile("(\\d+,?\\d*)\\*(\\d+,?\\d*)");
     private Pattern patternDivide = Pattern.compile("(\\d+)/(\\d+)");
-    private Pattern patternJustANumber = Pattern.compile("(\\d+)");
+    private Pattern patternJustANumber = Pattern.compile("(\\d+,?\\d*)");
+
+    private NumberFormat format = NumberFormat.getInstance(Locale.FRANCE);
 
     @Override
     public String getResponse(String query) {
         String calculateQuery = query.replace(' ', '+');
 
-        calculateQuery = calculateWithParenthesis(calculateQuery);
+        try {
+            calculateQuery = calculateWithParenthesis(calculateQuery);
+        } catch (ParseException e) {
+            Throwables.propagate(e);
+        }
 
         Matcher matcherJustANumber = patternJustANumber.matcher(calculateQuery);
 
@@ -27,12 +39,80 @@ public class CalculateQueryHandler extends AbstractQueryHandler {
         return null;
     }
 
-    private String calculateWithParenthesis(String calculateQuery) {
+    private String calculateWithParenthesis(String calculateQuery) throws ParseException {
         Matcher matcherParenthsis = patternParenthesis.matcher(calculateQuery);
 
         while (matcherParenthsis.find()) {
-            int start=-1;
-            int end=-1;
+            SearchParanthesis searchParanthesis = new SearchParanthesis(calculateQuery).invoke();
+            int start = searchParanthesis.getStart();
+            int end = searchParanthesis.getEnd();
+
+            String queryBetweenParenthesis = calculateQuery.substring(start+1, end-1);
+            System.out.println(queryBetweenParenthesis);
+            String result = calculateWithoutParenthesis(queryBetweenParenthesis);
+            calculateQuery = calculateQuery.substring(0, start) + result + calculateQuery.substring(end);
+            matcherParenthsis = patternParenthesis.matcher(calculateQuery);
+        }
+
+        calculateQuery = calculateWithoutParenthesis(calculateQuery);
+        return calculateQuery;
+    }
+
+    private String calculateWithoutParenthesis(String calculateQuery) throws ParseException {
+        System.out.println(calculateQuery);
+        Matcher matcherMultiple = patternMultiple.matcher(calculateQuery);
+
+        while (matcherMultiple.find()) {
+            double a = format.parse(matcherMultiple.group(1)).doubleValue();
+            double b = format.parse(matcherMultiple.group(2)).doubleValue();
+            double result = a*b;
+            calculateQuery = calculateQuery.substring(0, matcherMultiple.start()) + format.format(result) + calculateQuery.substring(matcherMultiple.end());
+            matcherMultiple = patternMultiple.matcher(calculateQuery);
+        }
+
+        Matcher matcherDivide = patternDivide.matcher(calculateQuery);
+
+        while (matcherDivide.find()) {
+            double a = format.parse(matcherDivide.group(1)).doubleValue();
+            double b = format.parse(matcherDivide.group(2)).doubleValue();
+            double result = a / b;
+            calculateQuery = calculateQuery.substring(0, matcherDivide.start()) + format.format(result) + calculateQuery.substring(matcherDivide.end());
+            matcherDivide = patternPlus.matcher(calculateQuery);
+        }
+
+        Matcher matcherPlus = patternPlus.matcher(calculateQuery);
+
+        while (matcherPlus.find()) {
+            double a = format.parse(matcherPlus.group(1)).doubleValue();
+            double b = format.parse(matcherPlus.group(2)).doubleValue();
+            double result = a + b;
+            calculateQuery = calculateQuery.substring(0, matcherPlus.start()) + format.format(result) + calculateQuery.substring(matcherPlus.end());
+            matcherPlus = patternPlus.matcher(calculateQuery);
+        }
+        return calculateQuery;
+    }
+
+
+    private class SearchParanthesis {
+        private String calculateQuery;
+        private int start;
+        private int end;
+
+        public SearchParanthesis(String calculateQuery) {
+            this.calculateQuery = calculateQuery;
+        }
+
+        public int getStart() {
+            return start;
+        }
+
+        public int getEnd() {
+            return end;
+        }
+
+        public SearchParanthesis invoke() {
+            start = -1;
+            end = -1;
             int currentProf=0;
             int currentStart=-1;
             int maxProf=-1;
@@ -52,51 +132,7 @@ public class CalculateQueryHandler extends AbstractQueryHandler {
                     currentProf--;
                 }
             }
-
-            String queryBetweenParenthesis = calculateQuery.substring(start+1, end-1);
-            System.out.println(queryBetweenParenthesis);
-            int result = Integer.parseInt(calculateWithoutParenthesis(queryBetweenParenthesis));
-            calculateQuery = calculateQuery.substring(0, start) + result + calculateQuery.substring(end);
-            matcherParenthsis = patternParenthesis.matcher(calculateQuery);
+            return this;
         }
-
-        calculateQuery = calculateWithoutParenthesis(calculateQuery);
-        return calculateQuery;
     }
-
-    private String calculateWithoutParenthesis(String calculateQuery) {
-        System.out.println(calculateQuery);
-        Matcher matcherMultiple = patternMultiple.matcher(calculateQuery);
-
-        while (matcherMultiple.find()) {
-            int a = Integer.parseInt(matcherMultiple.group(1));
-            int b = Integer.parseInt(matcherMultiple.group(2));
-            int result = a*b;
-            calculateQuery = calculateQuery.substring(0, matcherMultiple.start()) + result + calculateQuery.substring(matcherMultiple.end());
-            matcherMultiple = patternMultiple.matcher(calculateQuery);
-        }
-
-        Matcher matcherDivide = patternDivide.matcher(calculateQuery);
-
-        while (matcherDivide.find()) {
-            int a = Integer.parseInt(matcherDivide.group(1));
-            int b = Integer.parseInt(matcherDivide.group(2));
-            int result = a / b;
-            calculateQuery = calculateQuery.substring(0, matcherDivide.start()) + result + calculateQuery.substring(matcherDivide.end());
-            matcherDivide = patternPlus.matcher(calculateQuery);
-        }
-
-        Matcher matcherPlus = patternPlus.matcher(calculateQuery);
-
-        while (matcherPlus.find()) {
-            int a = Integer.parseInt(matcherPlus.group(1));
-            int b = Integer.parseInt(matcherPlus.group(2));
-            int result = a + b;
-            calculateQuery = calculateQuery.substring(0, matcherPlus.start()) + result + calculateQuery.substring(matcherPlus.end());
-            matcherPlus = patternPlus.matcher(calculateQuery);
-        }
-        return calculateQuery;
-    }
-
-
 }
