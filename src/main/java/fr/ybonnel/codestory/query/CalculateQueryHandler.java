8b0505edfecc3748f142dfcf96b481abbf6edb99
ybonnel <ybonnel@gmail.com>
2013-01-11 20:56:47 +0100

    Add parenthesis managment

--- src/main/java/fr/ybonnel/codestory/query/CalculateQueryHandler.java
+++ src/main/java/fr/ybonnel/codestory/query/CalculateQueryHandler.java
@@ -7,24 +7,52 @@ import java.util.regex.Pattern;
 public class CalculateQueryHandler extends AbstractQueryHandler {
 
     private Pattern patternPlus = Pattern.compile("(\\d+)\\+(\\d+)");
+    private Pattern patternPlusWithParenthesis = Pattern.compile("\\((\\d+)\\+(\\d+)\\)");
     private Pattern patternMultiple = Pattern.compile("(\\d+)\\*(\\d+)");
+    private Pattern patternJustANumber = Pattern.compile("(\\d+)");
 
     @Override
     public String getResponse(String query) {
         String calculateQuery = query.replace(' ', '+');
 
-        Matcher matcherPlus = patternPlus.matcher(calculateQuery);
-        if (matcherPlus.matches()) {
-            int a = Integer.parseInt(matcherPlus.group(1));
-            int b = Integer.parseInt(matcherPlus.group(2));
-            return Integer.toString(a + b);
+        Matcher matcherPlusWithParenthesis = patternPlusWithParenthesis.matcher(calculateQuery);
+
+        while (matcherPlusWithParenthesis.find()) {
+            int a = Integer.parseInt(matcherPlusWithParenthesis.group(1));
+            int b = Integer.parseInt(matcherPlusWithParenthesis.group(2));
+            int result = a + b;
+            calculateQuery = calculateQuery.substring(0, matcherPlusWithParenthesis.start()) + result + calculateQuery.substring(matcherPlusWithParenthesis.end());
+            matcherPlusWithParenthesis = patternPlus.matcher(calculateQuery);
         }
+
         Matcher matcherMultiple = patternMultiple.matcher(calculateQuery);
-        if (matcherMultiple.matches()) {
+
+        while (matcherMultiple.find()) {
             int a = Integer.parseInt(matcherMultiple.group(1));
             int b = Integer.parseInt(matcherMultiple.group(2));
-            return Integer.toString(a * b);
+            int result = a*b;
+            calculateQuery = calculateQuery.substring(0, matcherMultiple.start()) + result + calculateQuery.substring(matcherMultiple.end());
+            matcherMultiple = patternMultiple.matcher(calculateQuery);
         }
+
+        Matcher matcherPlus = patternPlus.matcher(calculateQuery);
+
+        while (matcherPlus.find()) {
+            int a = Integer.parseInt(matcherPlus.group(1));
+            int b = Integer.parseInt(matcherPlus.group(2));
+            int result = a + b;
+            calculateQuery = calculateQuery.substring(0, matcherPlus.start()) + result + calculateQuery.substring(matcherPlus.end());
+            matcherPlus = patternPlus.matcher(calculateQuery);
+        }
+
+        Matcher matcherJustANumber = patternJustANumber.matcher(calculateQuery);
+
+        if (matcherJustANumber.matches()) {
+            return calculateQuery;
+        }
+
         return null;
     }
+
+
 }
--- src/test/java/fr/ybonnel/codestory/CalculateTest.java
+++ src/test/java/fr/ybonnel/codestory/CalculateTest.java
@@ -14,31 +14,47 @@ public class CalculateTest extends WebServerTestUtil {
 
     @Test
     public void should_answer_to_1plus1() throws IOException, SAXException {
-        WebConversation wc = new WebConversation();
-        WebResponse response = wc.getResponse(getURL() + "/?q=1+1");
-        assertEquals(200, response.getResponseCode());
-        assertEquals("2", response.getText());
+        test_calculate("1+1", "2");
     }
 
     @Test
     public void should_answer_to_allPlus() throws IOException, SAXException {
         for (int number = 1; number < 10; number++) {
-            WebConversation wc = new WebConversation();
-            WebResponse response = wc.getResponse(getURL() + "/?q=" + number + "+" + number);
-            assertEquals(200, response.getResponseCode());
-            assertEquals(Integer.toString(number+number), response.getText());
+            test_calculate(Integer.toString(number) + "+" + number, Integer.toString(number + number));
         }
     }
 
     @Test
     public void should_answer_to_allMultiple() throws IOException, SAXException {
         for (int number = 1; number < 10; number++) {
-            WebConversation wc = new WebConversation();
-            WebResponse response = wc.getResponse(getURL() + "/?q=" + number + "*" + number);
-            assertEquals(200, response.getResponseCode());
-            assertEquals(Integer.toString(number*number), response.getText());
+            test_calculate(Integer.toString(number) + "*" + number, Integer.toString(number * number));
         }
     }
+
+    private void test_calculate(String query, String resultExpected) throws IOException, SAXException {
+        WebConversation wc = new WebConversation();
+        WebResponse response = wc.getResponse(getURL() + "/?q=" + query);
+        assertEquals(200, response.getResponseCode());
+        assertEquals(resultExpected, response.getText());
+    }
+
+    @Test
+    public void should_answer_to_more_complicated() throws IOException, SAXException {
+        test_calculate("1+2*2", "5");
+    }
+
+    @Test
+    public void should_answer_with_parenthesis() throws IOException, SAXException {
+        test_calculate("(1+2)*2", "6");
+//        (1+2)*2
+//        (1+2)/2
+//        (1+2+3+4+5+6+7+8+9+10)*2
+//        ((1+2)+3+4+(5+6+7)+(8+9+10)*3)/2*5
+//        1,5*4
+//        ((1,1+2)+3,14+4+(5+6+7)+(8+9+10)*4267387833344334647677634)/2*553344300034334349999000
+//        ((1,1+2)+3,14+4+(5+6+7)+(8+9+10)*4267387833344334647677634)/2*553344300034334349999000/31878018900000000000000000000000000000000000000000
+    }
+
 
 
 
