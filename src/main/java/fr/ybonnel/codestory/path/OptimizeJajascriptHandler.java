8affe3505ca82f568ef165923c3ff4bd5d89d070
ybonnel <ybonnel@gmail.com>
2013-01-15 14:26:28 +0100

    Add cache for jajascript

--- src/main/java/fr/ybonnel/codestory/path/OptimizeJajascriptHandler.java
+++ src/main/java/fr/ybonnel/codestory/path/OptimizeJajascriptHandler.java
@@ -9,22 +9,35 @@ import fr.ybonnel.codestory.path.jajascript.JajascriptService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 public class OptimizeJajascriptHandler extends AbstractPathHandler {
 
     private final ObjectMapper mapper = new ObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL);
 
+    private Map<String, String> mapReponses = new HashMap<String, String>();
+
 
     @Override
     public PathResponse getResponse(HttpServletRequest request, String payLoad, String... params) throws Exception {
 
-        JajascriptRequest jajascriptRequest = JajascriptRequest.fromPayLoad(payLoad);
+        String reponse;
 
-        JajaScriptResponse jajaScriptResponse = new JajascriptService(jajascriptRequest).calculate();
+        if (mapReponses.containsKey(payLoad)) {
+            reponse = mapReponses.get(payLoad);
+        } else {
 
+            JajascriptRequest jajascriptRequest = JajascriptRequest.fromPayLoad(payLoad);
 
+            JajaScriptResponse jajaScriptResponse = new JajascriptService(jajascriptRequest).calculate();
 
-        PathResponse pathResponse = new PathResponse(HttpServletResponse.SC_OK, mapper.writeValueAsString(jajaScriptResponse));
+            reponse = mapper.writeValueAsString(jajaScriptResponse);
+
+            mapReponses.put(payLoad, reponse);
+        }
+
+        PathResponse pathResponse = new PathResponse(HttpServletResponse.SC_OK, reponse);
         pathResponse.setContentType("application/json");
         return pathResponse;
     }
